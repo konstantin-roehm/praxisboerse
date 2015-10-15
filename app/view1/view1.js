d5c68ce127819a84adb73d31e819a83fb143e71d
Konstantin Röhm <mythos0@gmx.de>
2015-10-15 16:54:45 +0200

    Rudimentäre Loginmöglichkeit eingebaut. Es wird das verschlüsselte Passwort vom Server zurückgegeben.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,5 @@
 'use strict';
-
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute','base64'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -8,7 +7,27 @@ angular.module('myApp.view1', ['ngRoute'])
     controller: 'View1Ctrl'
   });
 }])
+.config(['$httpProvider',function($httpProvider) {
+// Cross-Domain-Aufrufe erlauben
+      $httpProvider.defaults.useXDomain = true;
+// Das Mitsenden von Authentifizierungsinformationen erlauben
+      $httpProvider.defaults.withCredentials = true;
+}])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl',['$scope','$log','$http','$base64', function($scope,$log,$http,$base64) {
+        var root = 'http://www.iwi.hs-karlsruhe.de/Intranetaccess/REST';
+        $scope.login = function(){
+            $log.log('hallo');
+            $http.defaults.headers.common.Authorization = "Basic " + $base64.encode($scope.username + ":" + $scope.password);
+            var url = root +'/credential/encryptedpassword'; //gibt verschlüsseltes pw wieder
+            $http({method:'GET',url:url,headers:{'Accept': 'text/plain'}}).then(function(data) {
+                $log.log(data);
+                if(data.data.length==0){
+                    alert('Falsche Zugangsdaten');
+                } else {
+                    var encPw = data.data;
+                    location.href = '#/view2';
+                }
+            });
+        };
 }]);
